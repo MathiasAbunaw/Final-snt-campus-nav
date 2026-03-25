bf67027ba864a005b2a463c2ea99f4c597f49a18
Arreyebot Mathias Abunaw <aabunaw2006@gmail.com>
2026-03-25 16:19:20 -0500

    graph.py created inorder to store the nodes and edges of MST and to also add shortcut routes within the graph as optional edges. and router.py created inorder to evalute retrieve the query coordinates use them inorder to find the shortest route from one building to another

---
+++ BackEnd/routes/router.py
@@ -0,0 +1,35 @@
+from graph import load_graph #importing the function for load_graph to access the nodes and edges within G
+import networkx as nx #inorder to use Dijkastra function to find the shortest route
+import osmnx as ox
+from fastapi import APIRouter #to create a mini router
+
+
+G = load_graph()
+
+route = APIRouter()
+#This endpoint accepts start and end coordinates as query parameters
+# React will call this URL: /route?start_lat=37.9516&start_lng=-91.7654&end_lat=37.9501&end_lng=-91.766
+@route.get("/route")
+def get_route(start_lat: float, start_lng: float, end_lat: float, end_lng : float ): #we declared parameter within the function because we want fastAPI to know what to check in the url so the variable name have to match 
+                                                            # exactly to what we will be seeing in the url. and the : indisde function means type hint, we are telling python what to expect as the input of the parameter 
+
+    #This will help find the nearest nodes in the graph close to the start and end coordinate
+    start_node = ox.nearest_node(G, start_lng, start_lat)
+    end_node = ox.nearest_node(G, end_lng, end_lat)
+
+    #This will find the shortest path giving the starting node and ending node  using the Dijkstra algoriths to find the shortest length 
+    path = nx.shortest_path(G, start_node, end_node, weight="length") #weight = 'length means we want networkX to add up the actual distance of edges not just count how many edges their are
+
+    #This will store all the coordinates of every node along the paths to get to the end node 
+
+    coordinate = []
+
+    for node in path:
+    
+        point = G.nodes[node]
+        coordinate.append([point['y'], point['x']])
+
+    #this will return a dictionary with values of list of all the coordinate point of every nodes from start coordiate to end coordinate 
+    return {"route": coordinate}
+
+
