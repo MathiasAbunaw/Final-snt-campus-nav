6edc2121b270ccca23931eae8e1194a0df315ad8
Arreyebot Mathias Abunaw <aabunaw2006@gmail.com>
2026-03-24 23:54:38 -0500

    geojson file created to store shortest route that stores edges given the start point and end point. and main.py connect those edges with the closest nodes and add them as an option for networkx to use

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import osmnx as ox
-
+import json
 #This is used to creat FastAPI app instance 
 
 app = FastAPI()
@@ -20,6 +20,28 @@ def read_root():
 G = ox.graph_from_place("Missouri University of Science and Technology, Rolla, Missouri", network_type = 'walk', retain_all = True)
 
 print("campus graph loaded completed")
-print(f'Nodes: {len(G.nodes)}') #this wll the the length of how many different node aka building are within S&T
-print(f'Endges: {len(G.edges)}') #This will het the length of how many different edges their are
+print(f'Nodes: {len(G.nodes)}') #this wll the the length of how many different node aka point on the graph. For example imagen that every corners on maps are represented as nodes and edges can only be straight so nodes points
+print(f'Endges: {len(G.edges)}') #This will be the length of how many different edges their are. Edges are straight paths that connects from node to node
 
+
+#this all allow us to open and read the shortcut file we created 
+with open("shortcuts.geojeson") as f:
+    shortcuts = json.load(f)
+
+#this for loop is used inorder to looped through all of the feature key within the json file and find all the shortcuts and then add them as new edges that can be used within the graph 
+for feature in shortcuts["features"]:
+    coords = feature["geometry"]["cooridinates"]
+
+    #get the start and end coordinate of the short cut
+
+    start = coords[0] #this will get the start coordinate within the shortcut json file with its [longitude, latitude]
+    end = coords[-1] #this will get the end coordinate within the shortcut json file with it [longitude, latitude]
+    
+
+    #This will find the nearest existing nodes in the graph to connect the new edges we just created too
+    start_node = ox.nearest_nodes(G, start[0], start[1]) #This will connect the start edge to the closest node we can find
+    end_node = ox.nearest_nodes(G, end[0], end[1]) #this will connect the end edge with the closest node we can find
+    
+    #This will add the new shortcut as a new edges between those nodes
+
+    G.add_edge(start_node, end_node, name=feature["properties"]["name"], shortcuts= True)
