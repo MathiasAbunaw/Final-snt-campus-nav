26080ed58a5ba2d0d97f0d7ba76a9243867c0d77
Arreyebot Mathias Abunaw <aabunaw2006@gmail.com>
2026-03-24 21:46:24 -0500

    Phase 1B - FastAPI backend with OSMNX campus graph

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import osmnx as ox
 
 #This is used to creat FastAPI app instance 
 
@@ -9,3 +10,16 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return{"message": "S&T Campus Nav API is running"}
+
+#Download S&T campus walkway netwrok from openstreetMap
+
+#network_type= 'walk' means we only want pedestrians paths not the driveable ones 
+
+#retain_all = True - keeps all paths including small ones
+
+G = ox.graph_from_place("Missouri University of Science and Technology, Rolla, Missouri", network_type = 'walk', retain_all = True)
+
+print("campus graph loaded completed")
+print(f'Nodes: {len(G.nodes)}') #this wll the the length of how many different node aka building are within S&T
+print(f'Endges: {len(G.edges)}') #This will het the length of how many different edges their are
+
